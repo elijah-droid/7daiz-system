1834311eb4a297176e16043564cd824fe4da14c6
elijah-droid <mukisaelijah293@gmail.com>
2024-01-07 15:58:42 +0300

    updates

--- bizadmin/settings.py
+++ bizadmin/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,8 +102,17 @@ WSGI_APPLICATION = 'bizadmin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'edumetrics_test_database',
+        'USER': 'edumetrics_user',
+        'PASSWORD': 'intention12@',
+        'HOST': 'edu-metrics.com',  # Set the host where your MySQL server is running
+        'PORT': '3306',       # Set the port for your MySQL server
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
